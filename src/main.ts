241c7185388f1c099221d3e037b3678bc9a7b3df
Raul Correia <raulvinicius245@gmail.com>
2022-05-23 11:48:53 -0300

    Adicionado validationPipe ao main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      //recusa requisições daquilo que esta fora da whitelist
+      forbidNonWhitelisted: true,
+      //transforma os dados da requisição de acordo com o DTO
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
