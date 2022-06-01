e092d64a5024d3bd42f53cc99b3b9f0b6cae15ba
Raul Correia <raulvinicius245@gmail.com>
2022-06-01 10:23:21 -0300

    Criando Post baseado no email do author.

--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -3,14 +3,46 @@ import { CreatePostDto } from './../dto/create-post.dto';
 import { PrismaService } from './../../prisma/prisma.service';
 import { PostEntity } from '../entities/post.entity';
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { NotFoundError } from 'src/commom/errors/types/NotFoundError';
 
 @Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createPostDTO: CreatePostDto): Promise<PostEntity> {
+    //Destructering para pegar o email do autor vindo do create DTO de Post.
+    const { authorEmail } = createPostDTO;
+
+    //Deletando o email do objeto do DTO, para não causar conflito com o schema do prisma
+    //que não possui email para Post.
+    delete createPostDTO.authorEmail;
+
+    //Buscando Usuario (para capturar o ID), pelo email.
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    //Caso não encontre o usuario com o email passado pelo DTO
+    if (!user) {
+      throw new NotFoundError('User not found.');
+    }
+
+    //Spread para concatenar as informações do DTO para fazer o create.
+    const data: Prisma.PostCreateInput = {
+      ...createPostDTO,
+      author: {
+        //conecta a criação do post ao author com o email passado
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
     return this.prisma.post.create({
-      data: createPostDTO,
+      data: data,
     });
   }
 
