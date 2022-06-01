0f050a1f0de3e327f7daf72b3ce9ac0dde9dd825
Raul Correia <raulvinicius245@gmail.com>
2022-06-01 11:00:51 -0300

    Retornando posts em rotas de usuario, adicionado tratativa de erro para update.

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -3,6 +3,7 @@ import { UpdateUserDto } from './../dto/update-user.dto';
 import { PrismaService } from './../../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
+import { NotFoundError } from 'src/commom/errors/types/NotFoundError';
 
 @Injectable()
 export class UsersRepository {
@@ -11,28 +12,64 @@ export class UsersRepository {
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
     return this.prisma.user.create({
       data: createUserDto,
+      include: {
+        posts: {
+          select: {
+            title: true,
+            createdAt: true,
+          },
+        },
+      },
     });
   }
 
   async findAll(): Promise<UserEntity[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      include: {
+        posts: {
+          select: {
+            title: true,
+            createdAt: true,
+          },
+        },
+      },
+    });
   }
 
   async findOne(id: number): Promise<UserEntity> {
-    // By unique identifier
     return this.prisma.user.findUnique({
-      where: {
-        id: id,
+      where: { id: id },
+      include: {
+        posts: {
+          select: {
+            title: true,
+            createdAt: true,
+          },
+        },
       },
     });
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: id },
+    });
+
+    if (!user) {
+      throw new NotFoundError('User not found.');
+    }
+
     return this.prisma.user.update({
-      where: {
-        id: id,
-      },
+      where: { id: id },
       data: updateUserDto,
+      include: {
+        posts: {
+          select: {
+            title: true,
+            createdAt: true,
+          },
+        },
+      },
     });
   }
 
